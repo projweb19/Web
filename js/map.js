42e8c71037ff39dc88944ac9d911547d685763c1
Deartheload <k.eke123@outlook.fr>
2019-03-27 16:10:21 +0100

    Ajout d'un pushpin unique sur la carte qui récupère la location au clique et au déplacement

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 var map;
-var pushpin;
+var pushpin = null;
 function loadMapScenario() {
 
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
@@ -14,23 +14,27 @@ function loadMapScenario() {
       minZoom: 3
     });
 
-    Microsoft.Maps.Events.addHandler(map, 'click',getLatlng ); 
+    Microsoft.Maps.Events.addHandler(map, 'click',createPushPin );
 
+    Microsoft.Maps.Events.addHandler(pushpin, 'dragend', function () { highlight(); });
 
 }
 
-function getLatlng(e) {
-        if (e.targetType == "map") {
-           var point = new Microsoft.Maps.Point(e.getX(), e.getY());
-           var locTemp = e.target.tryPixelToLocation(point);
-           var location = new Microsoft.Maps.Location(locTemp.latitude, locTemp.longitude);
-         alert(locTemp.latitude+"&"+locTemp.longitude);
+function createPushPin (e) {
+  if (e.targetType == "map") {
+     var point = new Microsoft.Maps.Point(e.getX(), e.getY());
+     var locTemp = e.target.tryPixelToLocation(point);
+     var location = new Microsoft.Maps.Location(locTemp.latitude, locTemp.longitude);
 
+     map.entities.remove(pushpin);
+     pushpin = new Microsoft.Maps.Pushpin(location, {'draggable': true});
 
-           var pin = new Microsoft.Maps.Pushpin(location, {'draggable': false});
+       map.entities.push(pushpin);
 
-             map.entities.push(pin);
-             alert("Done");
+  }
 
-        }
-       }
+}
+
+function highlight(e) {
+  alert("&");
+}
